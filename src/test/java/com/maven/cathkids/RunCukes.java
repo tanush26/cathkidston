9801dad14ff8ab26abbc0a0584e9d14a9647e952
“srilatha24” <darsanamsrilatha@gmail.com>
2019-10-21 12:13:30 +0100

    added src

--- src/test/java/com/maven/cathkids/RunCukes.java
+++ src/test/java/com/maven/cathkids/RunCukes.java
@@ -5,6 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources",dryRun = false,strict = true,tags = "@register",plugin = {"json:target/srilatha.json"})
+@CucumberOptions(features = "src/test/resources",dryRun = false,strict = true,tags = "@register",//plugin = {"json:target/srilatha.json"})
+plugin = {"json:src/test/resources.json", "html:src/test/reports/cucumberReport.html"})
 public class RunCukes {
 }
